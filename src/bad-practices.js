284a14b33c07289b08fcd546fb1ace4b50461222
Łukasz Zarzecki <Lukasz_Zarzecki@mckinsey.com>
2019-12-17 10:41:39 +0100

    Initial commit

--- src/bad-practices.js
+++ src/bad-practices.js
@@ -16,27 +16,44 @@ const users = [
     {userId: 3, name: 'Anna'},
 ];
 
+app.get('/articles/', (req, res) => {
+    // Don't send plaintext JSON!
+    // Make sure you return the proper media type!
+    return res.send(JSON.stringify(article))
+});
+
 /* ...
 *  GET calls should NOT modify data!*/
-app.get('/article/:articleId/publish', (req, res) => {
+app.get('/articles/:articleId/publish', (req, res) => {
     article.isPublished = true;
     return res.json(article)
 });
 
-// Unnecessary endpoints for CRUD
-app.post('/article/create', (req, res) => {});
-app.get('/article/list', (req, res) => {});
-app.put('/article/:articleId/edit', (req, res) => {});
-app.delete('/article/:articleId/delete', (req, res) => {});
-app.get('/article/:articleId/get', (req, res) => {});
+// Using POST for both creating and updating
+app.post('/articles/1', (req, res) => {});
 
+// Unnecessary endpoints for CRUD, just stick to HTTP methods.
+app.post('/articles/create', (req, res) => {});
+app.get('/articles/list', (req, res) => {});
+app.put('/articles/:articleId/edit', (req, res) => {});
+app.delete('/articles/:articleId/delete', (req, res) => {});
+app.get('/articles/:articleId/get', (req, res) => {});
+
+// Use HTTP codes! Not custom errors!
+// Replying with a 200 OK response and then informing in the body there's an error is terrible!
 app.get('/some/resource', (req, res) => {
     try {
-        return undefinedCallThatWillThrow()
+        return undefinedCallThatWillThrow() // or validation errors
     } catch (e) {
         return res.json({error_code: 7})
     }
 });
 
+// Using POST as GET with filtering
+app.post('/users/', (req, res) => {
+    // apply some filter condition based on req.body
+    const users = users.filter(u => u)
+    return res.json(users)
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/index.js
+++ src/index.js
@@ -10,31 +10,75 @@ const users = [
     {userId: 3, name: 'Anna'},
 ];
 
+const messages = [
+    {userId: 1, messageId: 1, message: 'Lorem'},
+    {userId: 1, messageId: 2, message: 'Ipsum'},
+    {userId: 1, messageId: 3, message: 'Solor'},
+    {userId: 2, messageId: 4, message: 'It'},
+    {userId: 2, messageId: 5, message: 'Damet'}
+];
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-// GET on resource group for listing data
-// curl localhost:3000/users
-app.get('/users', (req, res) => res.json(users));
+// GET on resource collection
+// curl localhost:3000/api/users
+app.get('/api/users', (req, res) => res.json(users));
 
-// POST for creating a new user
-// curl -X POST localhost:3000/users -d '{"name":"Lucas"}' -H "Content-Type: application/json"
-app.post('/users', (req, res) => {
+// POST for creating a member resource (new user)
+// curl -X POST localhost:3000/api/users -d '{"name":"Lucas"}' -H "Content-Type: application/json"
+app.post('/api/users', (req, res) => {
     const newUser = {
-        userId: users.length +1,
+        userId: users.length + 1,
         name: req.body.name
     };
     users.push(newUser);
     return res.json(newUser)
 });
 
-// GET on specific resource for details
-app.get('/users/:userId', (req, res) => {
+// GET on specific member resource for details
+// curl localhost:3000/api/users/1
+app.get('/api/users/:userId', (req, res) => {
     return res.json(users.find(u => u.userId === +req.params.userId))
 });
 
-// Update our member resource
-app.patch('/users/:userId', (req, res) => res.send([]));
+// PUT Replace our member resource
+// curl -X PUT localhost:3000/api/users/1 -d '{"name":"UpdateUser"}' -H "Content-Type: application/json"
+// curl -X PUT localhost:3000/api/users/10 -d '{"name":"MyNewUser"}' -H "Content-Type: application/json"
+app.put('/api/users/:userId', (req, res) => {
+    let userIdx = users.findIndex(u => u.userId === +req.params.userId);
+    if (userIdx === -1) {
+        // If requested element does not exist, PUT will create a new member
+        users.push({
+            userId: users.length +1,
+            ...req.body,
+        })
+    }
+    users[userIdx] = {
+        ...users[userIdx],
+        ...req.body
+    };
+    return res.json(users[userIdx])
+});
 
-app.get('/users/:userId/messages', (req, res) => res.send([]));
+// PATCH Update our member resource
+// curl -X PATCH localhost:3000/api/users/1 -d '{"newProp":"hellothere"}' -H "Content-Type: application/json"
+app.patch('/api/users/:userId', (req, res) => {
+    let userIdx = users.findIndex(u => u.userId === +req.params.userId);
+    if (userIdx === -1) {
+        // If requested element does not exist, PATCH may be used to create a new resource
+        // This is not mandatory, but REST allows us that.
+    }
+    users[userIdx] = {
+        ...users[userIdx],
+        ...req.body
+    };
+    return res.json(users[userIdx])
+});
+
+// GET collection of a member resource
+// curl localhost:3000/api/users/1/messages
+app.get('/api/users/:userId/messages', (req, res) => {
+    return res.json(messages.filter(m => m.userId === +req.params.userId))
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
